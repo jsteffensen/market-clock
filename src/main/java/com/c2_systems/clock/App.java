74e4f4d555076f7ec514c5a146cc5fff653a08ca
jsteffensen <jsteffensen@gmail.com>
2020-08-12 15:11:58 +0200

    fix minor stuff

--- src/main/java/com/c2_systems/clock/App.java
+++ src/main/java/com/c2_systems/clock/App.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Calendar;
 import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,7 +16,7 @@ import javax.swing.JTextField;
 public class App extends JFrame {
 
 
-    	 private JTextField displayField;  // set by timer listener
+    	 private JTextField displayField;
 
     	    public App() {
 
@@ -23,7 +25,7 @@ public class App extends JFrame {
     	        
     	        displayField = new JTextField(5);
     	        displayField.setEditable(false);
-    	        displayField.setFont(new Font("sansserif", Font.PLAIN, 48));
+    	        displayField.setFont(new Font("sansserif", Font.PLAIN, 96));
 
     	        JPanel content = new JPanel();
     	        content.setLayout(new FlowLayout());
@@ -35,31 +37,28 @@ public class App extends JFrame {
     	        this.setLocationRelativeTo(null);
     	        this.setResizable(false);
 
-    	        javax.swing.Timer t = new javax.swing.Timer(100, new ClockListener());
-    	        
-    	        t.start();
-    	    }
-    	    
-    	    /////////////////////////////////////////////// inner class ClockListener
-    	    class ClockListener implements ActionListener {
-    	    	public void actionPerformed(ActionEvent e) {
+	    	    TimerTask task = new TimerTask() {
+	    	    	public void run() {
+	
+	    	            Calendar now = Calendar.getInstance();
+	    	            int h = now.get(Calendar.HOUR_OF_DAY);
+	    	            
+	    	            int m = now.get(Calendar.MINUTE);
+	    	            int s = now.get(Calendar.SECOND);
+	    	            
+	    	            String paddedHour = String.format("%02d", h);
+	    	            String paddedMinute = String.format("%02d", m);
+	    	            String paddedSecond = String.format("%02d", s);
+	    	            
+	    	            displayField.setText(paddedHour + ":" + paddedMinute + ":" + paddedSecond);
+	    	    	}
+	    	    };
+    	    	
+    	    	new Timer().scheduleAtFixedRate(task, 0, 100);
 
-    	            Calendar now = Calendar.getInstance();
-    	            int h = now.get(Calendar.HOUR_OF_DAY);
-    	            
-    	            int m = now.get(Calendar.MINUTE);
-    	            int s = now.get(Calendar.SECOND);
-    	            
-    	            String paddedHour = String.format("%02d", h);
-    	            String paddedMinute = String.format("%02d", m);
-    	            String paddedSecond = String.format("%02d", s);
-    	            
-    	            displayField.setText(paddedHour + ":" + paddedMinute + ":" + paddedSecond);
-
-    	    	}
     	    }
+    	   
     	    
-    	    //================================================================= main
     	    public static void main(String[] args) {
     	        JFrame clock = new App();
     	        clock.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
